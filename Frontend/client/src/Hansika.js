e7124a54f183c3487c6131ab9d1ceb0b7009bbeb
Thidas <161629129+thidaswick@users.noreply.github.com>
2025-04-30 16:16:24 +0530

    update new one

--- Frontend/client/src/Hansika.js
+++ Frontend/client/src/Hansika.js
@@ -1,3 +1,9 @@
 //Hansika
 //Banne adareta
 //wdiya hitanna epa
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
