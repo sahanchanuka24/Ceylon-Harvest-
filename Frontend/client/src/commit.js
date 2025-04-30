c5284de8b4eeb86ae763f4a4fa6ddfb2f2e16cf2
Thidas <161629129+thidaswick@users.noreply.github.com>
2025-04-30 15:43:29 +0530

    react

--- Frontend/client/src/commit.js
+++ Frontend/client/src/commit.js
@@ -4,3 +4,15 @@
 // therunda
 //hakii nam codes danna
 //chat gpt aran menna me wge
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
