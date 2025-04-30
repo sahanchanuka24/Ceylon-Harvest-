125531a5fbdb583e33001d49e4f61b67475ff5bd
Thidas <161629129+thidaswick@users.noreply.github.com>
2025-04-30 16:17:01 +0530

    Update Venusha.js

--- Frontend/client/src/Venusha.js
+++ Frontend/client/src/Venusha.js
@@ -1,3 +1,9 @@
 //venusha 
 //khmda
 //kalada
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
