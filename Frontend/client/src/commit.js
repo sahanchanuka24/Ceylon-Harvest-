8b9c3710eb4e1456333d87aa69c76b5fceed667e
Thidas <161629129+thidaswick@users.noreply.github.com>
2025-04-30 16:20:59 +0530

    Update commit.js

--- Frontend/client/src/commit.js
+++ Frontend/client/src/commit.js
@@ -25,3 +25,29 @@ ReactDOM.render (
     </Provider> 
   
 );
+ React.createElement(
+          MenuItem,
+          { onClick: handleMenuClose },
+          React.createElement(
+            ListItemIcon,
+            null,
+            React.createElement(PersonIcon, { fontSize: 'small', sx: { color: '#26580F' } })
+          ),
+          React.createElement(ListItemText, null, 'Profile')
+        ),
+        React.createElement(
+          MenuItem,
+          { onClick: handleLogout },
+          React.createElement(
+            ListItemIcon,
+            null,
+            React.createElement(LogoutIcon, { fontSize: 'small', sx: { color: '#26580F' } })
+          ),
+          React.createElement(ListItemText, null, 'Logout')
+        )
+      )
+    )
+  );
+}
+
+export default Navbar;
