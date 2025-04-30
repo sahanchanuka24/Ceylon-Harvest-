ed499f33e29b2c69b15d79eeef5d2f00f441b4f8
Thidas <161629129+thidaswick@users.noreply.github.com>
2025-04-30 16:20:01 +0530

    Update Hansika.js

--- Frontend/client/src/Hansika.js
+++ Frontend/client/src/Hansika.js
@@ -7,3 +7,30 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+ React.createElement(
+          MenuItem,
+          { onClick: handleMenuClose },
+          React.createElement(
+            ListItemIcon,
+            null,
+            React.createElement(PersonIcon, { fontSize: 'small', sx: { color: '#26580F' } })
+          ),
+          React.createElement(ListItemText, null, 'Profile')
+        ),
+        React.createElement(
+          MenuItem,
+          { onClick: handleLogout },
+          React.createElement(
+            ListItemIcon,
+            null,
+            React.createElement(LogoutIcon, { fontSize: 'small', sx: { color: '#26580F' } })
+          ),
+          React.createElement(ListItemText, null, 'Logout')
+        )
+      )
+    )
+  );
+}
+
+export default Navbar;
