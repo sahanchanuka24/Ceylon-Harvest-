f84b8783fde3b0968fa4a68af7868ddb15f6fd78
sahan chanuka <sahanchanuka90@gmail.com>
2025-04-30 15:33:58 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,34 @@
 //password:- 2npps70yajmpiK2q
 
-const express = require("express");
-const mongoose = require("mongoose");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const { errorHandler } = require('./utils/errorHandler');
+
+// Connect to database
+connectDB();
 
 const app = express();
 
-//Middleeare
-app.use("/",(req, res, next) => {
-    res.send("Thidas,Sahan,Venusha,Hansika");
-})
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
 
-mongoose.connect("mongodb+srv://admin:2npps70yajmpiK2q@cluster0.gf62g.mongodb.net/")
-.then(()=> console.log("Connected to MongoDB"))
-.then(() => {
-   app.listen(5000);
-})
-.catch ((err)=> console.log((err)));
+// Routes
+app.use('/api/auth', require('./Route/auth'));
+app.use('/api/users', require('./Route/users'));
+
+// Error Handler
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
