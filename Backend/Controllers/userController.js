89d2e2a70d8d960b47f08d1d74374de860299a4d
sahan chanuka <sahanchanuka90@gmail.com>
2025-04-30 15:35:11 +0530

    Create userController.js

---
+++ Backend/Controllers/userController.js
@@ -0,0 +1,75 @@
+const { ErrorResponse } = require('../utils/errorHandler');
+const User = require('../Model/User');
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+exports.getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({
+            success: true,
+            count: users.length,
+            data: users
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// @desc    Get single user
+// @route   GET /api/users/:id
+// @access  Private/Admin
+exports.getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return next(new ErrorResponse(`User not found with id of ${req.params.id}`, 404));
+        }
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+exports.updateUser = async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if (!user) {
+            return next(new ErrorResponse(`User not found with id of ${req.params.id}`, 404));
+        }
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+exports.deleteUser = async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return next(new ErrorResponse(`User not found with id of ${req.params.id}`, 404));
+        }
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (error) {
+        next(error);
+    }
+}; 
