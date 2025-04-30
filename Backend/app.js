fc226a992218aea852e49738b63d7cd55b49d442
sahan chanuka <sahanchanuka90@gmail.com>
2025-04-30 14:42:40 +0530

    Backend file

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+//password:- 2npps70yajmpiK2q
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Middleeare
+app.use("/",(req, res, next) => {
+    res.send("Thidas,Sahan,Venusha,Hansika");
+})
+
+mongoose.connect("mongodb+srv://admin:2npps70yajmpiK2q@cluster0.gf62g.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.then(() => {
+   app.listen(5000);
+})
+.catch ((err)=> console.log((err)));
